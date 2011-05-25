7c8307ff126407419d06764d0c69629e56e4f155
q10 <q101100111000@gmail.com>
2011-05-25 12:53:52 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: BENSON J MA
+ *
+ * Created on May 25, 2011, 12:50 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
