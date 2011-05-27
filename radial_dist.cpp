673510043590b9f0ea42ce8fb1f4965601ac74b9
q10 <q101100111000@gmail.com>
2011-05-27 03:39:57 -0700

    radial distribution function in place, temperature can be specified when running program, and some units corrected(not complete)

---
+++ radial_dist.cpp
@@ -0,0 +1,45 @@
+#include "common.h"
+
+static int num_gr, num_his_bars = 20;
+static double delg;
+double * radial_dist_data = new double [num_his_bars];
+
+void initialize_radial_dist_sampler() {
+    num_gr = 0;
+    delg = BOX_LENGTH / (2 * num_his_bars);
+    for (int i = 0; i < num_his_bars; i++)
+        radial_dist_data[i] = 0;
+    return;
+}
+
+void radial_dist_sample() {
+    num_gr++;
+    double dx, dy, dz, dr;
+    for (int i = 0; i < NUM_WATERS - 1; i++) {
+        for (int j = i + 1; j < NUM_WATERS; j++) {
+            dx = abs(water_positions[i][0] - water_positions[j][0]);
+            dy = abs(water_positions[i][1] - water_positions[j][1]);
+            dz = abs(water_positions[i][2] - water_positions[j][2]);
+            dx -= BOX_LENGTH * ROUND(dx / BOX_LENGTH);
+            dy -= BOX_LENGTH * ROUND(dy / BOX_LENGTH);
+            dz -= BOX_LENGTH * ROUND(dz / BOX_LENGTH);
+            dr = sqrt(dx * dx + dy * dy + dz * dz);
+            if (dr < BOX_LENGTH / 2.0) {
+                int ig = int(dr / delg);
+                radial_dist_data[ig] += 2;
+            }
+        }
+    }
+    return;
+}
+
+void compute_radial_dist_results() {
+    double r, vb, nid;
+    for (int i = 0; i < num_his_bars; i++) {
+        r = delg * (i + 0.5);
+        vb = (pow(i + 1, 3) - pow(i, 3)) * pow(delg, 3);
+        nid = (4 / 3) * M_PI * vb * WATER_DENSITY;
+        radial_dist_data[i] /= num_gr * NUM_WATERS * nid;
+    }
+    return;
+}
---
+++ radial_dist.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   radial_dist.h
+ * Author: BENSON J MA
+ *
+ * Created on May 27, 2011, 2:07 AM
+ */
+
+#ifndef RADIAL_DIST_H
+#define	RADIAL_DIST_H
+
+void initialize_radial_dist_sampler();
+void radial_dist_sample();
+void compute_radial_dist_results();
+
+#endif	/* RADIAL_DIST_H */
